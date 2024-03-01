ae470f7ad9c2d168ebf86a29b27becd25eb29745
jason <hyunjun306@gmail.com>
2024-02-29 22:02:54 -0500

    step2 fix the user model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 /** User class for message.ly */
 
-
-
+const { DB_URI } = require("../config");
+const bcrypt = require("bcrypt")
+const {BCRYPT_WORK_FACTOR} = require('../config')
+const db = require("../db")
 /** User of the site. */
 
 class User {
@@ -10,20 +12,54 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({username, password, first_name, last_name, phone}) { 
+    const hashedPw = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+      `INSERT INTO users
+      (username, password, first_name, last_name, phone, join_at, last_login_at) 
+      VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
+      RETURNING username, password, first_name, last_name, phone
+      `,
+      [username, hashedPw, first_name, last_name, phone]
+    );
+    //console.log(result)
+    return result.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) { 
+    const result = await db.query(`
+    SELECT username, password
+    FROM users WHERE username = $1`,
+    [username])
+    const user = result.rows[0]
+    if (user){
+      if(await bcrypt.compare(password,user.password)){
+        return true;
+      }
+    }
+    return false;
+  }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) { 
+    await db.query(`
+    UPDATE users
+    SET last_login_at = current_timestamp 
+    WHERE username =$1`,[ username]);
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() { 
+    const results = await db.query(`
+    SELECT username, first_name, last_name, phone
+    FROM users`)
+    return results.rows;
+  }
 
   /** Get: get user by username
    *
@@ -34,7 +70,14 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {
+    const result = await db.query(`
+    SELECT username, first_name, last_name, phone, join_at, last_login_at
+    FROM users
+    WHERE username=$1`,[username]);
+    //console.log(result.rows)
+    return result.rows[0];
+   }
 
   /** Return messages from this user.
    *
@@ -44,7 +87,29 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) {
+    const result = await db.query(`
+    SELECT m.id, m.body, m.sent_at, m.read_at,
+    u.username, u.first_name, u.last_name, u.phone
+    FROM messages m
+    JOIN users u
+    ON m.to_username = u.username
+    WHERE m.from_username=$1
+    `, [username])
+
+    return result.rows.map(row=>({
+      id: row.id,
+      body: row.body,
+      sent_at: row.sent_at,
+      read_at: row.read_at,
+      to_user: {
+        username : row.username,
+        first_name : row.first_name,
+        last_name : row.last_name,
+        phone : row.phone
+      }
+    }));
+   };
 
   /** Return messages to this user.
    *
@@ -54,7 +119,29 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+  static async messagesTo(username) {
+    const result = await db.query(`
+    SELECT m.id, m.body, m.sent_at, m.read_at,
+    u.username, u.first_name, u.last_name, u.phone
+    FROM messages m
+    JOIN users u
+    ON m.from_username = u.username
+    WHERE m.to_username=$1
+    `, [username])
+
+    return result.rows.map(row=>({
+      id: row.id,
+      body: row.body,
+      sent_at: row.sent_at,
+      read_at: row.read_at,
+      from_user: {
+        username : row.username,
+        first_name : row.first_name,
+        last_name : row.last_name,
+        phone : row.phone
+      }
+    }));
+   }
 }
 
 
